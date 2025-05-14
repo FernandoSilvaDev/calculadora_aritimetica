4e3dc7861d9f0d2d6faea373ed973a016b17e237
Fernando Silva <artfernando.carlos@gmail.com>
2025-05-14 15:44:49 -0300

    git inicial

---
+++ src/App.vue
@@ -0,0 +1,47 @@
+<script setup>
+
+import { ref, computed } from 'vue';
+import Cabecalho from './components/Cabecalho.vue'
+
+const num1 = ref(0);
+const num2 = ref(0);
+const operation = ref('add');
+
+const result = computed(() => {
+  switch (operation.value) {
+    case 'add':
+      return num1.value + num2.value;
+    case 'subtract':
+      return num1.value - num2.value;
+    case 'multiply':
+      return num1.value * num2.value;
+    case 'divide':
+      return num2.value !== 0 ? num1.value / num2.value : 'Erro: divisão por zero';
+    default:
+      return 0;
+  }
+});
+
+</script>
+
+<template>
+<div class="container">
+  <Cabecalho :num1="num1" 
+      :num2="num2" 
+      :operation="operation" 
+      @update:num1="num1 = $event"
+      @update:num2="num2 = $event"
+      @update:operation="operation = $event"
+/>
+      <h2>Resultado: {{ result }}</h2>
+  </div>
+
+</template>
+
+<style scoped>
+  input, select {
+    margin: 5px;
+    padding: 5px;
+  }
+
+</style>
---
+++ src/components/Cabecalho.vue
@@ -0,0 +1,23 @@
+<script setup>
+import { defineProps, defineEmits } from 'vue';
+
+defineProps(['num1', 'num2', 'operation']);
+const emit = defineEmits(['update:num1', 'update:num2', 'update:operation']);
+
+</script>
+<template>
+    <header class="p-5 mb-4 mt-4 bg-black rounded-4">
+    <h1 class="text-light mb-4">Calculadora Aritmética</h1>
+    <input class="ms-3" type="number" :value="num1" @input="emit('update:num1', +$event.target.value)" placeholder="Número 1">
+    <input class="ms-4" type="number" :value="num2" @input="emit('update:num2', +$event.target.value)" placeholder="Número 2">
+    <select class="ms-4 bg-primary" :value="operation" @change="emit('update:operation', $event.target.value)">
+    
+    <div class="list-group">
+        <option value="add">+</option>
+    <option value="subtract">-</option>
+    <option value="multiply">*</option>
+    <option value="divide">/</option>
+    </div>
+    </select>
+  </header>
+</template>
